bec7ebe11b76e0d8373fd7a88487953c94173c84
Ali Saglam <48090224+Ali-Saglam@users.noreply.github.com>
2021-01-18 15:47:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
 	/*---------V--------------V---------*/
 
 	smst mysmst(img);
-	mysmst.segmentation(0, 3);
+	mysmst.segmentation(200, 3);
 
 	/*----------------------------------*/
 	/*----------------------------------*/
